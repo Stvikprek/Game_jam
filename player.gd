9ea20d2200315d0ac50be915bdfe8f12a7fceae4
Stvikprek <b23411@students.iitmandi.ac.in>
2024-04-05 15:01:28 +0530

    Revamped Leaning

--- player.gd
+++ player.gd
@@ -5,15 +5,13 @@ extends Area3D
 @onready var scopetex= $scope_tex
 @onready var bullet_path = $RayCast3D
 var leaning = false
-const lean_amount = 0.07
+#const lean_amount = 0.07
 const lean_rot  = 40
-var return_pos = Vector3(0,0,0)
-var in_lean_dist = false
 var in_lean_rot = false
-var stuck = false	
+var stuck = false
+var moving:bool = false
 
 func _ready():
-	return_pos = position
 	scopetex.visible = false
 	Input.mouse_mode = (Input.MOUSE_MODE_CAPTURED)
 	
@@ -40,15 +38,7 @@ func _input(event):
 		rotation.y = clamp(rotation.y,deg_to_rad(-30),deg_to_rad(30))
 		
 func _process(delta):
-	if leaning == false and (in_lean_dist == true or in_lean_rot == true):
-		if position.x > return_pos.x + 0.1:
-			position.x -= lean_amount
-		elif position.x < return_pos.x - 0.1:
-			position.x += lean_amount
-		elif position.x < return_pos.x + 0.1 and position.x > return_pos.x - 0.1:
-			position = return_pos
-			return_pos = position
-			in_lean_dist = false
+	if leaning == false and (in_lean_rot == true):
 		if rotation.z < deg_to_rad(-2):
 			rotate_z(deg_to_rad(lean_rot/20))
 		elif rotation.z > deg_to_rad(2):
@@ -56,45 +46,34 @@ func _process(delta):
 		elif rotation.z > deg_to_rad(-2) and rotation.z < deg_to_rad(2):
 			rotation.z = deg_to_rad(0)
 			in_lean_rot = false
-
-		
 		
 	if Input.is_action_pressed("Lean Right"):
-		if position.x < return_pos.x + 2.5:
-			position.x += lean_amount
 		if rotation.z > deg_to_rad(-lean_rot):
 			rotate_z(deg_to_rad(-lean_rot/20))
-
 		leaning = true
-
-		in_lean_dist = true
 		in_lean_rot = true
+		
+
 	elif Input.is_action_just_released("Lean Right"):
 		leaning = false
 
 
 	
 	if Input.is_action_pressed("Lean Left"):
-		if position.x > return_pos.x - 2.5:
-			position.x -= lean_amount
 		if rotation.z < deg_to_rad(lean_rot):
 			rotate_z(deg_to_rad(lean_rot/20))
-
-
-		in_lean_dist = true
 		in_lean_rot = true
 		leaning = true
+
 	elif Input.is_action_just_released("Lean Left"):
 		leaning = false
 
-	if Input.is_action_pressed("Right") and in_lean_dist == false:
+	if Input.is_action_pressed("Right"):
 		position.x += 0.1
-		return_pos = position
-		
-	if Input.is_action_pressed("Left") and in_lean_dist == false:
+
+	if Input.is_action_pressed("Left"):
 		position.x -= 0.1
-		return_pos = position
-		
+
 	if Input.is_action_just_pressed("Shoot"):
 		if bullet_path.is_colliding():
 			var body = bullet_path.get_collider()
